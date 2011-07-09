7a606bd937719511570e8147b928d8d5f42280d4
shiftkey <me@brendanforster.com>
2011-07-09 12:26:51 +1000

    added packages to run tests against

---
+++ Tests/MahApps.RestBase.Tests/RestClientBaseTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Hammock;
+using Hammock.Authentication.OAuth;
+
+using MahApps.RESTBase;
+using MahApps.RESTBase.Delegates;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MahApps.RestBase.Tests
+{
+    [TestFixture]
+    public class RestClientBaseTests
+    {
+        [Test]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void BeginGetAccessToken_WithNoCredentialsSet_ThrowsException()
+        {
+            var mock = Substitute.For<IRestClient>();
+            var client = new RestClientBase(mock);
+
+            // client.Credentials = new OAuthCredentials() { Token = "ABC", TokenSecret = "DEF " };
+
+            bool isCallbackFired = false;
+            AccessTokenCallbackDelegate callback = (req, resp, o) => { isCallbackFired = true; };
+            client.BeginGetAccessToken(@"http://google.com/foo", callback);
+
+            Assert.Fail();
+        }
+
+        [Test]
+        public void BeginGetAccessToken_WithCredentialsSet_ThrowsException()
+        {
+            const string verifier = @"http://google.com/foo";
+
+            var mock = Substitute.For<IRestClient>();
+            var client = new RestClientBase(mock);
+
+            client.Credentials = new OAuthCredentials() { Token = "ABC", TokenSecret = "DEF " };
+
+            bool isCallbackFired = false;
+            AccessTokenCallbackDelegate callback = (req, resp, o) => { isCallbackFired = true; };
+            client.BeginGetAccessToken(verifier, callback);
+
+            var modifiedCredentials = client.Credentials as OAuthCredentials;
+
+            Assert.That(modifiedCredentials.Type, Is.EqualTo(OAuthType.AccessToken));
+            Assert.That(modifiedCredentials.Verifier, Is.EqualTo(verifier));
+        }
+    }
+}
